79ef3a70934ecdde7ada167052a8c8721ea416f4
Curtis Webb <curtwebb1194@@gmail.com>
2023-09-12 12:06:52 -0400

    Last model was created

---
+++ Models/BookReview.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookClub.Models
+{
+    public class BookReview
+    {
+        public int ReviewID { get; set; }
+
+        [Required(ErrorMessage = "BookID is required")]
+        public int BookID { get; set; }
+
+        [Required(ErrorMessage = "UserID is required")]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "ReviewText is required")]
+        public string ReviewText { get; set; }
+
+        [Required(ErrorMessage = "Rating is required")]
+        public int Rating { get; set; }
+    }
+}
